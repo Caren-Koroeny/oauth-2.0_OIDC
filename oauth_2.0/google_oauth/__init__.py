333b45c6bc3d449729ddb22c3fbd98d37e8ffe8b
Caren-Koroeny <carensiya5@gmail.com>
2022-06-09 11:25:16 +0300

    My first commit

---
+++ oauth_2.0/google_oauth/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+# from google_oauth.routes import views
+from google_oauth import config
+
+def create_app():
+    app=Flask(__name__)
+    app.config['FLASK_ENV'] = 'development'
+    app.config.from_object(config.Config)
+
+    
+    with app.app_context():
+        
+        from google_oauth.routes import views
+        app.register_blueprint(views)
+    # where the routes and blue prints will be initialized and called. 
+    
+        
+        
+        
+        
+        
+        
+    return app
+    
---
+++ oauth_2.0/google_oauth/config.py
@@ -0,0 +1,10 @@
+from os import environ
+
+
+class Config:
+
+    SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_APP = environ.get('FLASK_APP')
+    FLASK_ENV = environ.get('FLASK_ENV')
+
+# print(Config.FLASK_APP)
